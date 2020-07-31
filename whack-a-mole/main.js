d9e10b49bffcc0391ba1554351055d8036945769
egorodrigogn <egorodrigogn@gmail.com>
2020-07-30 23:56:36 -0300

    first commit

---
+++ whack-a-mole/main.js
@@ -0,0 +1,50 @@
+//TODOs
+//Make it go faster as time goes by, or at least faster
+//Style
+//Hit animation
+//Replace alert for something (or at least style alert)
+
+const square = document.querySelectorAll('.square')
+const mole = document.querySelectorAll('.mole')
+const timeLeft = document.querySelector('#time-left')
+let score = document.querySelector('#score')
+let result = 0
+let currentTime = timeLeft.textContent
+
+function randomSquare() {
+    square.forEach(className => {
+        className.classList.remove('mole')
+    })
+    let randomPosition = square[Math.floor(Math.random()*9)]
+    randomPosition.classList.add('mole')
+    //assign the id of a randomPosition to hitPosition for us to use later
+    hitPosition = randomPosition.id
+}
+
+square.forEach(id => {
+    id.addEventListener('mouseup', () => {
+        if(id.id === hitPosition) {
+            result = result + 1
+            score.textContent = result
+        }
+    })
+})
+
+function moveMole() {
+    let timerId= null
+    timerId = setInterval(randomSquare, 1000)
+}
+
+moveMole()
+
+function countDown() {
+    currentTime--
+    timeLeft.textContent = currentTime
+
+    if(currentTime === 0) {
+        clearInterval(timerId)
+        alert('GAME OVER! Your final score is' + result)
+    }
+}
+
+let timerId = setInterval (countDown, 1000)
